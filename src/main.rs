1d8ca3fb32349b7fe36e72406230224d3f2593c4
Marcus Xu <xumarcus.sg@gmail.com>
2023-12-09 18:13:38 +0800

    Tidy up

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use std::io;
+#[allow(unused_imports)]
+use std::{io, fs::File, path::Path, time::Duration};
 
 use tsp_ils::{Solver, TSP};
 
 fn main() -> io::Result<()> {
-    /*
-    let t = File::open(Path::new("tests/sample.tsp"))
-        .and_then(TSP::parse_tsp_file)?;
-     */
+    // let t = File::open(Path::new("src/data/berlin52.tsp")).and_then(TSP::parse_tsp_file)?;
     let t = TSP::parse_kattis()?;
-    let mut s = Solver::new(&t);
+    let mut s = Solver::new(&t, Duration::from_secs(2), 4234);
     s.run();
     for x in s.solution().seq() {
         println!("{}", x);
