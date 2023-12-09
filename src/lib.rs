4fec8793987c56f48f9cd80abb878017fb1f2cf0
Marcus Xu <xumarcus.sg@gmail.com>
2023-12-09 18:13:45 +0800

    Perturb

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use std::mem::swap;
 use std::ops::Range;
 use std::time::{Duration, Instant};
 type Point = (f64, f64);
@@ -12,20 +13,25 @@ pub fn d_pt(s: &Point, t: &Point) -> f64 {
 }
 
 pub struct LCG {
-    x: u64,
+    x: usize,
 }
 
 impl LCG {
-    const A: u64 = 1103515245;
-    const C: u64 = 12345;
+    const A: usize = 1103515245;
+    const C: usize = 12345;
 
-    pub fn new(seed: u64) -> Self {
+    pub fn new(seed: usize) -> Self {
         LCG { x: seed }
     }
 
-    pub fn gen(&mut self) -> u64 {
-        self.x = Self::A * self.x + Self::C;
+    pub fn gen(&mut self) -> usize {
+        self.x = Self::A.wrapping_mul(self.x).wrapping_add(Self::C);
         self.x
+    }
+
+    pub fn gen_by(&mut self, r: Range<usize>) -> usize {
+        let n = r.end - r.start + 1;
+        r.start + self.gen() % n // ignore modulo bias
     }
 }
 
@@ -47,29 +53,30 @@ impl<'a> Tour<'a> {
                 .and_then(|&a| {
                     (0..tsp.n)
                         .filter(|&i| !used[i] && i != a)
-                        .min_by_key(|&i| tsp.d_ix(a, i))
+                        .min_by_key(|&i| tsp.d_id(a, i))
                 })
                 .unwrap_or(0);
             seq.push(x);
             used[x] = true;
         }
-        Self::from_seq(tsp, seq)
-    }
-
-    pub fn from_seq(tsp: &'a TSP, seq: Vec<NodeId>) -> Self {
-        let mut pos: Vec<usize> = vec![0; tsp.n];
-        let mut cost = 0;
-        for (i, &x) in seq.iter().enumerate() {
-            let y = *seq.get(i + 1).unwrap_or(&seq[0]);
-            pos[x] = i;
-            cost += tsp.d_ix(x, y);
-        }
+        let mut pos = vec![0; tsp.n];
+        let cost = Self::from_seq(tsp, &seq, &mut pos);
         Self {
             tsp,
             seq,
             pos,
             cost,
         }
+    }
+
+    fn from_seq(tsp: &'a TSP, seq: &[NodeId], pos: &mut [usize]) -> i64 {
+        let mut cost = 0;
+        for (i, &x) in seq.iter().enumerate() {
+            let y = *seq.get(i + 1).unwrap_or(&seq[0]);
+            pos[x] = i;
+            cost += tsp.d_id(x, y);
+        }
+        cost
     }
 
     pub fn inc(&self, i: usize) -> usize {
@@ -91,11 +98,19 @@ impl<'a> Tour<'a> {
         }
     }
 
-    pub fn next_of(&self, t: NodeId) -> NodeId {
+    pub fn seq(&self) -> &[NodeId] {
+        &self.seq
+    }
+
+    pub fn cost(&self) -> i64 {
+        self.cost
+    }
+
+    pub fn next_id(&self, t: NodeId) -> NodeId {
         self.seq[self.inc(self.pos[t])]
     }
 
-    pub fn prev_of(&self, t: NodeId) -> NodeId {
+    pub fn prev_id(&self, t: NodeId) -> NodeId {
         self.seq[self.dec(self.pos[t])]
     }
 
@@ -111,7 +126,8 @@ impl<'a> Tour<'a> {
     }
 
     pub fn reverse(&mut self, x: NodeId, y: NodeId) {
-        let m = self.seglen(x, y).div_ceil(2);
+        // (unstable) let m = self.seglen(x, y).div_ceil(2);
+        let m = (self.seglen(x, y) + 1) / 2;
         let mut i = self.pos[x];
         let mut j = self.pos[y];
         for _ in 0..m {
@@ -123,12 +139,29 @@ impl<'a> Tour<'a> {
         }
     }
 
-    pub fn seq(&self) -> &[NodeId] {
-        &self.seq
-    }
+    pub fn perturb(&mut self, rng: &mut LCG, sbuf: &mut Vec<NodeId>, pbuf: &mut Vec<usize>) {
+        let n = self.tsp.n;
 
-    pub fn cost(&self) -> i64 {
-        self.cost
+        if n > 8 {
+            let i1 = rng.gen_by(1..n / 4);
+            let i3 = rng.gen_by(1..n / 4) + i1;
+            let i5 = rng.gen_by(1..n / 4) + i3;
+            // i7
+
+            sbuf.clear();
+            for slice in [
+                &self.seq[..i1],
+                &self.seq[i5..],
+                &self.seq[i3..i5],
+                &self.seq[i1..i3],
+            ] {
+                sbuf.extend_from_slice(slice);
+            }
+
+            self.cost = Self::from_seq(&self.tsp, sbuf, pbuf);
+            swap(&mut self.seq, sbuf);
+            swap(&mut self.pos, pbuf);
+        }
     }
 }
 
@@ -141,7 +174,7 @@ impl Timer {
     pub fn new(duration: Duration) -> Self {
         Self {
             t: Instant::now(),
-            duration,
+            duration: duration - Duration::from_millis(50),
         }
     }
 
@@ -166,16 +199,17 @@ impl<'a, 'b> Iterator for Opt2<'a, 'b> {
     type Item = i64;
     fn next(&mut self) -> Option<Self::Item> {
         let tour = &mut self.tour;
-        let d = |i, j| tour.tsp.d_ix(i, j);
+        let d = |i, j| tour.tsp.d_id(i, j);
         let n = tour.tsp.n;
 
         while let Some(t1) = self.r.next() {
             for dir in [true, false] {
                 let t2 = if dir {
-                    tour.next_of(t1)
+                    tour.next_id(t1)
                 } else {
-                    tour.prev_of(t1)
+                    tour.prev_id(t1)
                 };
+
                 for &t3 in tour.tsp.c[t1].iter() {
                     let g1 = d(t1, t2) - d(t1, t3);
                     if g1 < 0 {
@@ -183,10 +217,11 @@ impl<'a, 'b> Iterator for Opt2<'a, 'b> {
                     }
 
                     let t4 = if dir {
-                        tour.next_of(t3)
+                        tour.next_id(t3)
                     } else {
-                        tour.prev_of(t3)
+                        tour.prev_id(t3)
                     };
+
                     if t1 != t4 && t2 != t3 {
                         let g2 = d(t3, t4) - d(t2, t4);
                         let g = g1 + g2;
@@ -217,28 +252,37 @@ impl<'a, 'b> Iterator for Opt2<'a, 'b> {
 
 pub struct Solver<'a> {
     timer: Timer,
-    // rng: LCG,
+    rng: LCG,
     cur: Tour<'a>,
     bst: Tour<'a>,
 }
 
 impl<'a> Solver<'a> {
-    pub fn new(tsp: &'a TSP) -> Self {
+    pub fn new(tsp: &'a TSP, duration: Duration, seed: usize) -> Self {
         let t = Tour::new(&tsp);
         Self {
-            timer: Timer::new(Duration::from_secs(2)),
-            // rng: LCG::new(4234),
+            timer: Timer::new(duration),
+            rng: LCG::new(seed),
             cur: t.clone(),
             bst: t,
         }
     }
 
     pub fn run(&mut self) {
+        let n = self.cur.tsp.n;
+        let mut sbuf: Vec<NodeId> = Vec::with_capacity(n);
+        let mut pbuf: Vec<usize> = vec![0; n];
+
         while self.timer.ok() {
-            // self.cur.perturb(&mut self.rng);
-            Opt2::new(&mut self.cur)
-                .take_while(|_| self.timer.ok())
-                .for_each(drop);
+            self.cur.perturb(&mut self.rng, &mut sbuf, &mut pbuf);
+
+            let mut opt2 = Opt2::new(&mut self.cur);
+            while self.timer.ok() {
+                if opt2.next() == None {
+                    break;
+                }
+            }
+
             // Opt3::new(&mut self.cur)
             //     .take_while(|_| self.timer.ok());
             if self.cur.cost < self.bst.cost {
@@ -267,7 +311,7 @@ impl TSP {
         let n = p.len();
         let m: Vec<i64> = p
             .iter()
-            .flat_map(|s| p.iter().map(|t| d_pt(s, t).round() as i64))
+            .flat_map(|s| p.iter().map(move |t| d_pt(s, t).round() as i64))
             .collect();
         let c = (0..n)
             .map(|i| {
@@ -306,7 +350,7 @@ impl TSP {
         Self::parse_tsp(stdin.lock(), true)
     }
 
-    pub fn d_ix(&self, i: usize, j: usize) -> i64 {
+    pub fn d_id(&self, i: usize, j: usize) -> i64 {
         self.m[i * self.n + j]
     }
 }
@@ -348,14 +392,14 @@ mod tests {
             let j = (i + 1) % 10;
             let x = &POINTS[i];
             let y = &POINTS[j];
-            assert_eq!(tsp.d_ix(i, j), d_pt(x, y).round() as i64);
-            assert_eq!(tsp.d_ix(j, i), d_pt(y, x).round() as i64);
+            assert_eq!(tsp.d_id(i, j), d_pt(x, y).round() as i64);
+            assert_eq!(tsp.d_id(j, i), d_pt(y, x).round() as i64);
         }
     }
 
     #[test]
     fn test_tsp_parse_tsp() {
-        let path = Path::new("tests/sample.tsp");
+        let path = Path::new("src/data/sample.tsp");
         let tsp = File::open(path).and_then(TSP::parse_tsp_file).unwrap();
         let ans = [
             0, 74, 46, 48, 44, 28, 59, 98, 24, 58, 74, 0, 40, 26, 90, 66, 27, 25, 70, 24, 46, 40,
