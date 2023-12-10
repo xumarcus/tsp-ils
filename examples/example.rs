3d241ca88f6072d62992c650e037dbfd0c97789a
Marcus Xu <xumarcus.sg@gmail.com>
2023-12-10 20:45:51 +0800

    Add cnts to score bench

--- examples/example.rs
+++ examples/example.rs
@@ -13,40 +13,53 @@ use tsp_ils::{Solver, Tour, TSP};
 
 fn main() -> io::Result<()> {
     let optimal = HashMap::from([
-        (OsStr::new("d657.tsp"), 48912),
-        (OsStr::new("dsj1000.tsp"), 18659688),
-        (OsStr::new("lu980.tsp"), 11340),
-        (OsStr::new("uy734.tsp"), 79114),
-        (OsStr::new("zi929.tsp"), 95345),
+        ("d657.tsp", 48912),
+        ("dsj1000.tsp", 18659688),
+        ("lu980.tsp", 11340),
+        ("uy734.tsp", 79114),
+        ("zi929.tsp", 95345),
     ]);
 
+    let mut means: Vec<f64> = Vec::new();
     for entry in fs::read_dir(Path::new("examples/data"))? {
         let path = entry?.path();
         let file = File::open(path.clone())?;
         let tsp = TSP::parse_tsp_file(file)?;
 
-        let filename = path.file_name().expect(".. not allowed");
+        let filename = path.file_name().and_then(OsStr::to_str).expect("valid str");
         let opt = *optimal.get(filename).expect("no optimal recorded") as f64;
         let naive = Tour::new(&tsp).cost() as f64;
-        let results: Vec<f64> = (0..12)
+        let (cnts, scores): (Vec<_>, Vec<_>) = (0..12)
             .into_par_iter()
             .map(|seed| {
                 let mut s = Solver::new(&tsp, Duration::from_secs(2), seed);
-                s.run();
+                let cnt = s.run();
                 let val = s.solution().cost() as f64;
-                0.02_f64.powf((val - opt) / (naive - val))
+                (cnt as f64, 0.02_f64.powf((val - opt) / (naive - val)))
             })
-            .collect();
-        let data = Data::new(results);
+            .unzip();
+        let cnt_data = Data::new(cnts);
+        let score_data = Data::new(scores);
+        means.push(score_data.mean().unwrap());
 
         println!(
-            "{:>16?}: min={:.03} max={:.03} mean={:.03} stdev={:.03}",
+            "{:>16}: [cnt] min={:>6} max={:>6} mean={:>6.0} stdev={:>6.0} [val] min={:.03} max={:.03} mean={:.03} stdev={:.03}",
             filename,
-            data.min(),
-            data.max(),
-            data.mean().unwrap(),
-            data.std_dev().unwrap()
+            cnt_data.min(),
+            cnt_data.max(),
+            cnt_data.mean().unwrap(),
+            cnt_data.std_dev().unwrap(),
+            score_data.min(),
+            score_data.max(),
+            score_data.mean().unwrap(),
+            score_data.std_dev().unwrap()
         );
     }
+    let mean_data = Data::new(means);
+    println!(
+        "mean: {:.03} stdev={:.03}",
+        mean_data.mean().unwrap(),
+        mean_data.std_dev().unwrap()
+    );
     Ok(())
 }
